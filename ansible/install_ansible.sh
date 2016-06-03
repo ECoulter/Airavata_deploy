b5295b5e3d1f4105d2682d7d3c94e3d8e7345031
root <root@localhost.localdomain>
2016-06-03 13:27:01 -0400

    removed stuff in topdir; got ansible db setup working

--- ansible/install_ansible.sh
+++ ansible/install_ansible.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+yum -y install epel-release
 yum -y install python-devel python-pip python-yaml python-jinja2 gcc libffi-devel openssl-devel
 #pexpect has to be 3.3 because new 4.01 version only
 # works with python >= 2.7 :(
@@ -11,5 +12,8 @@ echo "# Ansible Inventory" > inventory
 echo "[headnode]" >> inventory
 echo "localhost ansible_connection=local" >> inventory
 mkdir -p /etc/ansible
-mv inventory /etc/ansible/hostsl
+
+mv inventory /etc/ansible/hosts
+echo -e "[defaults]" > /etc/ansible/ansible.cfg
+echo "retry_files_enabled = False # Do not create them" >> /etc/ansible/ansible.cfg
 ./ansible/bin/ansible headnode -a "/bin/hostname"
--- ansible/pre_install_start_services.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#rabbitmq
-#/opt/rabbitmq_server/sbin/rabbitmq-server start -detached
-
-systemctl start mariadb
-
-mysql_secure_installation
